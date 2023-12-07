22bba60a351e1afe9e04d7f892b716e8d2681775
denistred <denislylov2004@mail.ru>
2023-12-07 21:41:40 +0300

    new requirements and aiogram version

--- bot.py
+++ bot.py
@@ -2,23 +2,21 @@ import asyncio
 import logging
 import sys
 from aiogram import Bot, types, Dispatcher
-from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
-from aiogram.filters import CommandStart
 from config import TOKEN
 from scheduler import api_requests
 from threading import Thread
 
-dp = Dispatcher()
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-
-@dp.message(CommandStart())
+@dp.message_handler(commands=['start'])
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
-@dp.message()
+@dp.message_handler()
 async def echo_handler(message: types.Message) -> None:
     try:
         await message.send_copy(chat_id=message.chat.id)
@@ -27,7 +25,6 @@ async def echo_handler(message: types.Message) -> None:
 
 
 async def main() -> None:
-    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
 
 
